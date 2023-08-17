a76be4139d7c424dad42be9811f7c0d7d111c22c
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-08-17 16:27:51 +0200

    Remove GTest error from CMakeFiles

--- test/lexer_test.cpp
+++ test/lexer_test.cpp
@@ -1,8 +1,25 @@
 #include "gtest/gtest.h"
 
+#include <iostream>
+#include <sstream>
+
 #include "lexer.h"
 
 TEST(LexerTest, TestName) {
+    // Temporarily redirect cin to a string stream
+    std::istringstream new_cin{"fn fib ( a )"};
+    auto cin_rdbuf = std::cin.rdbuf(new_cin.rdbuf());
+
+    Lexer lexer;
+    Token token_res = lexer.getToken();
+    while (token_res) {
+        std::cerr << token_res << " ";
+        token_res = lexer.getToken();
+    }
+
+    // Reset cin and cout to normal.
+    std::cin.rdbuf(cin_rdbuf);
+
     EXPECT_EQ(3, 3);
 }
 
