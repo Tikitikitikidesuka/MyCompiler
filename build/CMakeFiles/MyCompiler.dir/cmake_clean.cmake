ce97fcc35dcc0917776a1b0808126b7bfe8f60c0
CLopMan <cesarlopman@gmail.com>
2023-08-17 15:50:50 +0200

    getToken update

---
+++ build/CMakeFiles/MyCompiler.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCompiler.dir/main.cpp.o"
+  "CMakeFiles/MyCompiler.dir/main.cpp.o.d"
+  "CMakeFiles/MyCompiler.dir/src/Lexer.cpp.o"
+  "CMakeFiles/MyCompiler.dir/src/Lexer.cpp.o.d"
+  "MyCompiler"
+  "MyCompiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyCompiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
