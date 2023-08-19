a1fedd6cf24281acec5446b844f3790e8387f101
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-08-19 16:10:11 +0200

    Add code to parser

--- include/parser.h
+++ include/parser.h
@@ -1,10 +1,23 @@
-
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <memory>
 
-class parser {
+#include "lexer.h"
+#include "abstract_syntax_tree.h"
 
+class Parser {
+private:
+    Lexer lexer;
+    TokenResult curr_token;
+
+public:
+    TokenResult getNextToken();
+    std::unique_ptr<ExprAST> logError(const std::string& error);
+    std::unique_ptr<PrototypeAST> logErrorP(const std::string& error);
+    std::unique_ptr<ExprAST> parseNumberExpr();
+    std::unique_ptr<ExprAST> parseParenthesisExpr();
+    std::unique_ptr<ExprAST> parseIdentifierExpr();
 };
 
 
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,1 +1,69 @@
 #include "parser.h"
+
+TokenResult Parser::getNextToken() {
+    this->curr_token = this->lexer.getToken();
+    return this->curr_token;
+}
+
+std::unique_ptr<ExprAST> logError(const std::string& error) {
+    std::cerr << "Error: " << error << "\n";
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> logErrorP(const std::string& error) {
+    this->logError(error);
+    return nullptr;
+}
+
+std::unique_ptr<ExprAST> Parser::parseNumberExpr() {
+    auto result = std::make_unique<NumberExprAST>(
+            stoi(this->curr_token.text));
+    this->getNextToken();
+    return std::move(result);
+}
+
+std::unique_ptr<ExprAST> Parser::parseParenthesisExpr() {
+    this->getNextToken();
+    auto ast = this->parseExpresion();
+
+    if (!ast)
+        return nullptr;
+
+    if (this->curr_token.token != Token::tok_cparenthesis)
+        return this->logError("Expected ')'");
+
+    this->getNextToken();
+    return ast;
+}
+
+std::unique_ptr<ExprAST> Parser::parseIdentifierExpr() {
+    std::string name = this->curr_token.text;
+
+    this->getNextToken();
+
+    if (this->curr_token.token != Token::tok_cparenthesis)
+        return std::make_unique<VariableExprAST>(name);
+
+    this->getNextToken();
+
+    std::vector<std::unique_ptr<ExprAST>> args;
+    if (this->curr_token.token != Token::tok_cparenthesis) {
+        while (true) {
+            auto arg = this->parseExpression();
+
+            if (arg) args.push_back(std::move(arg));
+            else return nullptr;
+
+            if (this->curr_token.token == Token::tok_cparenthesis) break;
+
+            if (this->curr_token.token != Token::tok_comma)
+                return this->logError("Expected ')' or ',' in argument list");
+
+            this->getNextToken();
+        }
+    }
+
+    this->getNextToken();
+
+    return std::make_unique<CallExprAST>(name, std::move(args));
+}
