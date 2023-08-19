8a18bf3890854170f13a9019d37e341f75131945
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-08-19 15:25:19 +0200

    Create Parser code files

---
+++ include/parser.h
@@ -0,0 +1,14 @@
+//
+// Created by Miguel on 19/8/23.
+//
+
+#ifndef MYCOMPILER_PARSER_H
+#define MYCOMPILER_PARSER_H
+
+
+class parser {
+
+};
+
+
+#endif //MYCOMPILER_PARSER_H
---
+++ src/parser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Miguel on 19/8/23.
+//
+
+#include "parser.h"
