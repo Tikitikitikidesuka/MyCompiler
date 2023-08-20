b22f37beb52e26a414456318882a4aa00f98898d
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-08-20 15:42:58 +0200

    Change Token Class hierarchy

--- include/lexer.h
+++ include/lexer.h
@@ -6,56 +6,68 @@
 #include <unordered_map>
 
 enum TokenType {
-    TOK_EOF,
-
-    TOK_ID,
     TOK_NUM,
+    TOK_ID,
+    TOK_RW,
+};
+
+enum RWTokenType {
+    TOK_EOF,
 
     TOK_ASSIGN,
     TOK_PLUS,
     TOK_MINUS,
     TOK_PARENTHESIS_OPEN,
     TOK_PARENTHESIS_CLOSE,
+
+    TOK_UNKOWN,
 };
+
+RWTokenType resolveReservedWord(const std::string& str) {
+    if (str == "+") return TOK_PLUS;
+    if (str == "-") return TOK_MINUS;
+    if (str == "=") return TOK_ASSIGN;
+    if (str == "(") return TOK_PARENTHESIS_OPEN;
+    if (str == ")") return TOK_PARENTHESIS_CLOSE;
+    return TOK_UNKOWN;
+}
 
 class Token {
 private:
     TokenType type;
 public:
     virtual ~Token() = default;
-    TokenType getType() {
-        return this->type;
-    }
+    Token(TokenType type) : type(type) {}
+    TokenType getType() { return this->type; }
 };
 
 class NumToken : public Token {
 private:
     int32_t num;
 public:
-    NumToken(int32_t num) : type(Token::TOK_NUM), num(num) {}
+    NumToken(int32_t num) : Token(TokenType::TOK_NUM), num(num) {}
 };
 
 class IdToken : public Token {
 private:
     std::string id;
 public:
-    IdToken(const std::string& id) : type(Token::TOK_ID), id(id) {}
+    IdToken(const std::string& id) : Token(TokenType::TOK_ID), id(id) {}
+};
+
+class RWToken : public Token {
+private:
+    RWTokenType rw_type;
+public:
+    RWToken(RWTokenType rw_type) : Token(TokenType::TOK_RW), rw_type(rw_type) {}
 };
 
 
 class Lexer {
 private:
     char state;
-    std::unordered_map<std::string, Token> str_to_token;
 public:
-    Lexer() {
-        this->str_to_token["+"] = Token::TOK_PLUS;
-        this->str_to_token["-"] = Token::TOK_MINUS;
-        this->str_to_token["="] = Token::TOK_ASSIGN;
-        this->str_to_token["("] = Token::TOK_PARENTHESIS_OPEN;
-        this->str_to_token[")"] = Token::TOK_PARENTHESIS_CLOSE;
-    }
-    TokenResult getToken();
+    std::unique_ptr<Token> getToken();
 };
 
 
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,6 +1,5 @@
 #include "lexer.h"
 
-#include <regex>
 #include <iostream>
 #include <vector>
 
@@ -32,10 +31,9 @@ std::unique_ptr<Token> Lexer::getToken() {
             input_buffer += input_char;
 
             // Check if token is a reserved word
-            for (const auto& token_test : this->str_to_token) {
-                if (input_buffer == token_test.first)
-                    return std::make_unique<Token>(RWToken(token_test.second));
-            }
+            RWTokenType rw_type = resolveReservedWord(input_buffer);
+            if (rw_type != RWTokenType::TOK_UNKOWN)
+                return std::make_unique<Token>(RWToken(rw_type));
 
             // Check if token is an identifier
             if (isalnum(input_char) && !isalnum(std::cin.peek()))
@@ -45,56 +43,5 @@ std::unique_ptr<Token> Lexer::getToken() {
         }
     }
 
-    return std::make_unique<Token>(Token(TokenType::TOK_UNKOWN));
+    return std::make_unique<Token>(RWToken(RWTokenType::TOK_UNKOWN));
 }
-
-/*
-TokenResult Lexer::getToken() {
-    std::string input_str = "";
-    char input = state;
-    switch (input)
-    {
-    //separators
-    case ':':
-        state = '\r';
-        return TokenResult(Token::tok_colon, ":");
-    case ';':
-        state = '\r';
-        return TokenResult(Token::tok_scolon, ";");
-    case '(':
-        state = '\r';
-        return TokenResult(Token::tok_oparenthesis, "(");
-    case ')':
-        state = '\r';
-        return TokenResult(Token::tok_cparenthesis, ")");
-    case '{':
-        state = '\r';
-        return TokenResult(Token::tok_okey, "{");
-    case '}':
-        state = '\r';
-        return TokenResult(Token::tok_ckey, "}");
-    case '[':
-        state = '\r';
-        return TokenResult(Token::tok_osquerebr, "[");
-     case ']':
-        state = '\r';
-        return TokenResult(Token::tok_csquerebr, "]");
-    default:
-        break;
-    }
-    
-    if (!(std::cin >> input)) return TokenResult(Token::tok_eof, input_str);
-    while (isalnum(input)) {
-        input_str += input;
-        std::cin >> input;
-    } 
-    this->state = input;
-
-    if (std::regex_match(input_str, std::regex("fn"))) return TokenResult(Token::tok_fn, input_str);
-    if (std::regex_match(input_str, std::regex("while"))) return TokenResult(Token::tok_while, input_str);
-    if (std::regex_match(input_str, std::regex("[a-zA-Z][a-zA-Z0-9]*"))) return TokenResult(Token::tok_identifier, input_str);
-    if (std::regex_match(input_str, std::regex("[0-9]+"))) return TokenResult(Token::tok_number, input_str);
-
-    return TokenResult(Token::tok_unknown, input_str);
-}
-*/
